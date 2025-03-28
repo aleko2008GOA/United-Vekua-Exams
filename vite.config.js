31464941833bdc7f84e11aa10b793e34e161172c
aleko2008GOA <alexmelikjanian@gmail.com>
2025-03-28 16:41:42 +0400

    version 0.0.2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
+  build: { outDir: 'dist', },
+  server: { historyApiFallback: true, },
 })
